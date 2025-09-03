6d933b9ecacd3ebdb46573ffd6f4e3534a2e3a8d
Gandolf0000 <joaovitor15oliveiracarvalho@gmail.com>
2025-09-03 18:05:15 +0000

    a

--- githubApi.js
+++ githubApi.js
@@ -2,7 +2,7 @@ async function getUser(username){
     const url = `https://api.github.com/users/${username}`;
     const response = await fetch(url);
 if (!response.ok){
-    throw new Error("Erro")
+    throw new Error("Usuário não encontrado")
 }
 return response.json();
 }
